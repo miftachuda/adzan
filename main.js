d2356a1ef3538062542d0590c81432d66eab8e93
Miftachul Huda <miftachulcc@gmail.com>
2024-03-01 21:04:40 +0700

    use play-sound

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const fs = require("fs");
-const symphonia = require("@tropicbliss/symphonia");
+const player = require("play-sound")();
 
-try {
-  const buf = fs.readFileSync("adzan.mp3"); // Gets a Buffer
-  symphonia.playFromBuf(buf, { speed: 1.0, volume: 1.0, isBlocking: true }); // The option object is optional. The speed and volume is both set to 1.0 and `isBlocking` is set to `true` by default.
-} catch (e) {
-  console.log("Error playing audio: ", e);
-}
+// Path to your music file
+const musicFile = "adzan.mp3";
+
+// Play music
+player.play(musicFile, (err) => {
+  if (err) {
+    console.error("Error playing music:", err);
+  } else {
+    console.log("Music has finished playing");
+  }
+});
